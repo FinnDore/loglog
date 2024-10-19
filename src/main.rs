31d84952b59de8560ea683641a02e2278d741366
Finn <34718806+FinnDore@users.noreply.github.com>
2024-10-19 16:59:56 +0100

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,18 @@
-//! # [Ratatui] Async example
-//!
-//! This example demonstrates how to use Ratatui with widgets that fetch data asynchronously. It
-//! uses the `octocrab` crate to fetch a list of pull requests from the GitHub API. You will need an
-//! environment variable named `GITHUB_TOKEN` with a valid GitHub personal access token. The token
-//! does not need any special permissions.
-//!
-//! <https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#creating-a-fine-grained-personal-access-token>
-//! <https://github.com/settings/tokens/new> to create a new token (select classic, and no scopes)
-//!
-//! This example does not cover message passing between threads, it only demonstrates how to manage
-//! shared state between the main thread and a background task, which acts mostly as a one-shot
-//! fetcher. For more complex scenarios, you may need to use channels or other synchronization
-//! primitives.
-//!
-//! A simple app might have multiple widgets that fetch data from different sources, and each widget
-//! would have its own background task to fetch the data. The main thread would then render the
-//! widgets with the latest data.
-//!
-//! The latest version of this example is available in the [examples] folder in the repository.
-//!
-//! Please note that the examples are designed to be run against the `main` branch of the Github
-//! repository. This means that you may not be able to compile with the latest release version on
-//! crates.io, or the one that you have installed locally.
-//!
-//! See the [examples readme] for more information on finding examples that match the version of the
-//! library you are using.
-//!
-//! [Ratatui]: https://github.com/ratatui/ratatui
-//! [examples]: https://github.com/ratatui/ratatui/blob/main/examples
-//! [examples readme]: https://github.com/ratatui/ratatui/blob/main/examples/README.md
 use std::{
     sync::{Arc, RwLock},
     time::Duration,
 };
 
 use color_eyre::{eyre::Context, owo_colors::OwoColorize, Result, Section};
+use crossterm::style::Stylize;
 use futures::StreamExt;
 
+use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
 use ratatui::{
     buffer::Buffer,
     crossterm::event::{Event, EventStream, KeyCode, KeyEventKind},
     layout::{Constraint, Layout, Rect},
-    style::{Style, Stylize},
+    style::{Color, Style},
     text::Line,
     widgets::{Block, HighlightSpacing, Row, StatefulWidget, Table, TableState, Widget},
     DefaultTerminal, Frame,
@@ -53,7 +24,8 @@ async fn main() -> Result<()> {
     let config = aws_config::load_from_env().await;
     let mut terminal = ratatui::init();
 
-    let app_result = App::default().run(terminal).await;
+    let app = App::default();
+    let app_result = app.run(terminal).await;
     ratatui::restore();
     app_result
 }
@@ -65,14 +37,14 @@ fn init_aws() -> Result<()> {
 #[derive(Debug, Default)]
 struct App {
     should_quit: bool,
-    pull_requests: LogGroupListWidget,
+    log_groups_component: LogGroupListComponent,
 }
 
 impl App {
     const FRAMES_PER_SECOND: f32 = 60.0;
 
     pub async fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
-        self.pull_requests.run();
+        self.log_groups_component.run();
 
         let period = Duration::from_secs_f32(1.0 / Self::FRAMES_PER_SECOND);
         let mut interval = tokio::time::interval(period);
@@ -90,16 +62,19 @@ impl App {
     fn draw(&self, frame: &mut Frame) {
         let vertical = Layout::vertical([Constraint::Length(1), Constraint::Fill(1)]);
         let [title_area, body_area] = vertical.areas(frame.area());
-        frame.render_widget(&self.pull_requests, body_area);
+        frame.render_widget(&self.log_groups_component, body_area);
     }
 
     fn handle_event(&mut self, event: &Event) {
+        let prevent_exit = self.log_groups_component.handle_event(event);
         if let Event::Key(key) = event {
             if key.kind == KeyEventKind::Press {
                 match key.code {
-                    KeyCode::Char('q') | KeyCode::Esc => self.should_quit = true,
-                    KeyCode::Char('j') | KeyCode::Down => self.pull_requests.scroll_down(),
-                    KeyCode::Char('k') | KeyCode::Up => self.pull_requests.scroll_up(),
+                    KeyCode::Char('q') | KeyCode::Esc => {
+                        if !prevent_exit {
+                            self.should_quit = true
+                        }
+                    }
                     _ => {}
                 }
             }
@@ -114,8 +89,11 @@ impl App {
 /// widget will clone the Arc, so you can pass it around to other threads, and this is used to spawn
 /// a background task to fetch the pull requests.
 #[derive(Debug, Clone, Default)]
-struct LogGroupListWidget {
+struct LogGroupListComponent {
     state: Arc<RwLock<LogGroupListState>>,
+    sorted_log_groups: Vec<String>,
+    search_term: String,
+    is_searching: bool,
 }
 
 #[derive(Debug, Default)]
@@ -141,11 +119,7 @@ enum LoadingState {
     Error(String),
 }
 
-impl LogGroupListWidget {
-    /// Start fetching the pull requests in the background.
-    ///
-    /// This method spawns a background task that fetches the pull requests from the GitHub API.
-    /// The result of the fetch is then passed to the `on_load` or `on_err` methods.
+impl LogGroupListComponent {
     fn run(&self) {
         let this = self.clone(); // clone the widget to pass to the background task
         tokio::spawn(this.fetch_pulls());
@@ -159,7 +133,7 @@ impl LogGroupListWidget {
 
         let config = aws_config::load_from_env().await;
         let client = aws_sdk_cloudwatchlogs::Client::new(&config);
-        let mut log_groups = match client.describe_log_groups().send().await {
+        let log_groups = match client.describe_log_groups().send().await {
             Ok(response) => Ok(response
                 .log_groups
                 .unwrap_or_default()
@@ -172,17 +146,17 @@ impl LogGroupListWidget {
 
         let mut state = self.state.write().unwrap();
         match log_groups {
-            Err(e) => {
-                state.loading_state = LoadingState::Error(e.to_string());
-                state.log_groups.clear();
-                return;
-            }
             Ok(groups) => {
                 state.loading_state = LoadingState::Loaded;
                 state.log_groups.extend(groups);
                 if !state.log_groups.is_empty() {
-                    state.table_state.select(Some(0));
+                    state.table_state.select_first();
                 }
+            }
+            Err(e) => {
+                state.loading_state = LoadingState::Error(e.to_string());
+                state.log_groups.clear();
+                return;
             }
         }
     }
@@ -194,30 +168,100 @@ impl LogGroupListWidget {
     fn scroll_up(&self) {
         self.state.write().unwrap().table_state.scroll_up_by(1);
     }
+
+    fn apply_search(&mut self) {
+        if self.search_term.is_empty() {
+            self.sorted_log_groups = self.state.read().unwrap().log_groups.clone();
+            return;
+        }
+        let groups = self.state.read().unwrap().log_groups.clone();
+        let matcher = SkimMatcherV2::default();
+        self.sorted_log_groups = groups
+            .into_iter()
+            .map(|group| {
+                (
+                    group.clone(),
+                    matcher.fuzzy_match(&group, &self.search_term),
+                )
+            })
+            .filter(|(_, score)| match score {
+                Some(score) => score > &5,
+                None => false,
+            })
+            .map(|(group, _)| group)
+            .collect();
+    }
+
+    fn handle_event(&mut self, event: &Event) -> bool {
+        if let Event::Key(key) = event {
+            if key.kind == KeyEventKind::Press {
+                match key.code {
+                    KeyCode::Down => self.scroll_down(),
+                    KeyCode::Up => self.scroll_up(),
+                    _ => (),
+                };
+            }
+            if self.is_searching {
+                match key.code {
+                    KeyCode::Esc => {
+                        self.is_searching = false;
+                        self.search_term.clear();
+                        self.sorted_log_groups = self.state.read().unwrap().log_groups.clone();
+                    }
+                    KeyCode::Backspace => {
+                        self.search_term.pop();
+                    }
+                    KeyCode::Char(c) => self.search_term.push(c),
+                    _ => (),
+                }
+                self.apply_search();
+                return key.code == KeyCode::Esc;
+            }
+            if key.kind == KeyEventKind::Press {
+                match key.code {
+                    KeyCode::Char('/') => self.is_searching = !self.is_searching,
+                    KeyCode::Char('j') => self.scroll_down(),
+                    KeyCode::Char('k') => self.scroll_up(),
+                    _ => (),
+                };
+            }
+        }
+        false
+    }
 }
 
-impl Widget for &LogGroupListWidget {
+impl Widget for &LogGroupListComponent {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let mut state = self.state.write().unwrap();
 
         // a block with a right aligned title with the loading state on the right
         let loading_state = Line::from(format!("{:?}", state.loading_state)).right_aligned();
+        let title = if self.is_searching {
+            Line::styled(
+                format!("/{}", self.search_term),
+                Style::new().fg(Color::Red),
+            )
+        } else {
+            Line::from("")
+        };
+        if self.is_searching {}
         let block = Block::bordered()
-            .title("Log Groups")
+            .title("Log Groups".to_string())
+            .title_bottom(title)
             .title(loading_state)
             .title_bottom(Line::from("q to quit").right_aligned());
 
         // a table with the list of pull requests
-        let rows = state
-            .log_groups
+        let rows = self
+            .sorted_log_groups
             .iter()
             .map(|log_group| Row::new(vec![log_group.to_string()]));
         let widths = [Constraint::Max(49)];
         let table = Table::new(rows, widths)
             .block(block)
             .highlight_spacing(HighlightSpacing::Always)
-            .highlight_symbol(">>")
-            .highlight_style(Style::new().red());
+            .highlight_symbol("🪵")
+            .highlight_style(Style::new().fg(Color::Red));
 
         StatefulWidget::render(table, area, buf, &mut state.table_state);
     }
