0485cbdb2f9760a68ceb0aaf2c37add9bdbaadbd
Finn <34718806+FinnDore@users.noreply.github.com>
2024-10-19 12:50:59 +0100

    \

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use std::io;
+use std::{
+    io,
+    sync::{Arc, RwLock},
+    time::Duration,
+};
 
 use clap::builder::Str;
+use crossterm::event::{Event, EventStream, KeyCode, KeyEventKind};
+use futures::StreamExt;
 use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
 use ratatui::{
-    crossterm::event::{self, KeyCode, KeyEventKind},
+    crossterm::event::{self},
     style::{Color, Style, Stylize},
     text::{Text, ToText},
     widgets::{List, Paragraph},
@@ -30,128 +36,134 @@ async fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
     let config = aws_config::load_from_env().await;
     let client = aws_sdk_cloudwatchlogs::Client::new(&config);
 
-    let mut log_groups: Vec<String> = match client.describe_log_groups().send().await {
-        Ok(response) => response
+    let mut log_selection = LogSelection::new(config).await;
+
+    log_selection
+        .run(terminal)
+        .await
+        .expect("Error running log selection component");
+    Ok(())
+}
+
+#[derive(Clone)]
+struct LogGroupState {
+    client: aws_sdk_cloudwatchlogs::Client,
+    logs: Vec<String>,
+    rest_call_state: RestCallState,
+}
+
+struct LogSelection {
+    log_group_state: Arc<RwLock<LogGroupState>>,
+    search_term: String,
+    is_searching: bool,
+    selected_index: usize,
+    matcher: SkimMatcherV2,
+    should_quit: bool,
+}
+
+impl LogSelection {
+    async fn new(config: aws_config::SdkConfig) -> Self {
+        let client = aws_sdk_cloudwatchlogs::Client::new(&config);
+
+        Self {
+            search_term: String::new(),
+            matcher: SkimMatcherV2::default(),
+            should_quit: false,
+            log_group_state: Arc::new(RwLock::new(LogGroupState {
+                client,
+                logs: vec![],
+                rest_call_state: RestCallState::LOADING,
+            })),
+            is_searching: false,
+            selected_index: 0,
+        }
+    }
+
+    async fn run(&mut self, mut terminal: DefaultTerminal) -> io::Result<()> {
+        let period = Duration::from_secs_f32(1.0 / 120.);
+        let mut interval = tokio::time::interval(period);
+        let mut events = EventStream::new();
+        tokio::spawn(fetch_log_groups(self.log_group_state.clone()));
+        while !self.should_quit {
+            tokio::select! {
+                _ = interval.tick() => { terminal.draw(|frame| self.draw(frame))?; },
+                Some(Ok(event)) = events.next() => self.handle_event(&event).await,
+            }
+        }
+
+        Ok(())
+    }
+
+    async fn draw(&mut self, frame: &mut ratatui::Frame<'_>) {
+        let state = self.log_group_state.read().unwrap();
+        if state.rest_call_state == RestCallState::LOADING {
+            let greeting = Paragraph::new("Loading...");
+            frame.render_widget(greeting, frame.area());
+        } else if state.rest_call_state == RestCallState::ERROR {
+            let greeting = Paragraph::new("Error loading groups");
+            frame.render_widget(greeting, frame.area());
+        } else {
+            let groups = List::new(
+                self.log_group_state
+                    .read()
+                    .unwrap()
+                    .logs
+                    .clone()
+                    .into_iter()
+                    .map(|g| Text::raw(g))
+                    .collect::<Vec<Text>>(),
+            );
+            frame.render_widget(groups, frame.area());
+        }
+    }
+
+    async fn handle_event(&mut self, event: &Event) {
+        if let Event::Key(key) = event {
+            if key.kind != KeyEventKind::Press {
+                return;
+            }
+            if key.code == KeyCode::Char('/') {
+                self.is_searching = !self.is_searching;
+            } else if key.code == KeyCode::Char('q') {
+                self.should_quit = true;
+            }
+            //  else if key.code == KeyCode::Down {
+            //     self.selected_index = (self.selected_index + 1).min(self.log_groups.len() - 1);
+            // } else if key.code == KeyCode::Up {
+            //     self.selected_index = (self.selected_index.max(1) - 1).max(0);
+            // }
+        }
+    }
+}
+
+async fn fetch_log_groups(state: Arc<RwLock<LogGroupState>>) {
+    let mut state = state.write().expect("Failed to lock state");
+    let mut log_groups = match state.client.describe_log_groups().send().await {
+        Ok(response) => Ok(response
             .log_groups
             .unwrap_or_default()
             .into_iter()
             .map(|group| group.log_group_name)
             .flatten()
-            .collect(),
-        Err(e) => panic!("Error: {:?}", e),
+            .collect::<Vec<String>>()),
+        Err(e) => Err(e),
     };
 
-    let selected_style = Style::default().fg(Color::LightMagenta);
-    loop {
-        if logs.is_empty() {
-            let mut groups = if search_term.is_empty() {
-                log_groups
-                    .clone()
-                    .into_iter()
-                    .map(|g| Text::raw(g))
-                    .collect::<Vec<Text>>()
-            } else {
-                matches
-                    .clone()
-                    .into_iter()
-                    .map(|(g, _)| Text::raw(g.clone()))
-                    .collect::<Vec<Text>>()
-            };
-            groups[selected_index] =
-                Text::styled(groups[selected_index].to_string(), selected_style.clone());
-            groups.push(Text::raw(format!("Searching for '{}'", search_term)));
-            groups.rotate_right(1);
-            terminal.draw(|frame| {
-                let greeting = List::new(groups);
-                frame.render_widget(greeting, frame.area());
-            })?;
-        } else {
-            let logs = List::new(
-                logs.clone()
-                    .into_iter()
-                    .map(|log| Text::raw(log))
-                    .collect::<Vec<Text>>(),
-            );
-            terminal.draw(|frame| {
-                frame.render_widget(logs, frame.area());
-            });
+    match log_groups {
+        Ok(groups) => {
+            state.logs = groups;
+            state.rest_call_state = RestCallState::OK;
         }
-        if let event::Event::Key(key) = event::read()? {
-            if key.code == KeyCode::Esc {
-                is_searching = false;
-                if !logs.is_empty() {
-                    logs.clear();
-                }
-                continue;
-            }
-            if key.kind != KeyEventKind::Press {
-                continue;
-            } else if is_searching {
-                selected_index = 0;
-                if key.code == KeyCode::Backspace {
-                    search_term.pop();
-                } else {
-                    search_term.push(key.code.to_string().chars().next().unwrap());
-                };
-                matches = log_groups
-                    .clone()
-                    .into_iter()
-                    .map(|group| (group.clone(), matcher.fuzzy_match(&group, &search_term)))
-                    .filter(|(_, score)| match score {
-                        Some(score) => score > &5,
-                        None => false,
-                    })
-                    .map(|(group, score)| (group, score.unwrap_or_default()))
-                    .collect::<Vec<_>>();
-            } else if key.code == KeyCode::Char('/') {
-                is_searching = !is_searching;
-            } else if key.code == KeyCode::Char('q') {
-                return Ok(());
-            } else if key.code == KeyCode::Down {
-                selected_index = (selected_index + 1).min(log_groups.len() - 1);
-            } else if key.code == KeyCode::Up {
-                selected_index = (selected_index.max(1) - 1).max(0);
-            } else if key.code == KeyCode::Enter {
-                let r = match client
-                    .start_query()
-                    .set_end_time(Some(chrono::Utc::now().timestamp_millis()))
-                    .set_start_time(Some(
-                        (chrono::Utc::now() - chrono::Duration::days(3000)).timestamp_millis(),
-                    ))
-                    .set_log_group_name(log_groups[selected_index].clone().into())
-                    .set_query_string(Some(
-                        "fields @timestamp, @message | sort @timestamp asc".to_owned(),
-                    ))
-                    .limit(500)
-                    .send()
-                    .await
-                {
-                    Ok(response) => response,
-                    Err(e) => panic!("Error: {:?}", e),
-                };
-
-                sleep(std::time::Duration::from_secs(15)).await;
-                logs = match client
-                    .get_query_results()
-                    .set_query_id(r.query_id)
-                    .send()
-                    .await
-                {
-                    Ok(response) => response
-                        .results
-                        .unwrap_or_default()
-                        .into_iter()
-                        .flatten()
-                        .map(|result| result.value.unwrap_or_default())
-                        .collect::<Vec<String>>(),
-                    Err(e) => panic!("Error: {:?}", e),
-                };
-                // .events
-                //                     .unwrap_or_default()
-                //                     .into_iter()
-                //                     .map(|event| event.message.unwrap_or_default())
-                //                     .collect::<Vec<String>>(),
-            }
+        Err(_) => {
+            state.logs = vec![];
+            state.rest_call_state = RestCallState::ERROR;
         }
     }
+}
+
+#[derive(PartialEq, Clone)]
+enum RestCallState {
+    OK,
+    ERROR,
+    LOADING,
 }
